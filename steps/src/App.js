a7e587c4b40078a2c5ad5f6edcc4ff715da5e0e7
Alan Webb <webbalan91939@gmail.com>
2024-10-22 00:40:44 +0100

    Update App component

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,16 +1,29 @@
+const messages = [
+	"Learn React ⚛️",
+	"Apply for jobs 💼",
+	"Invest your new income 🤑",
+];
+
 export default function App() {
+	const step = 1;
+
 	return (
 		<div className="steps">
 			<div className="numbers">
-				<div>1</div>
+				<div className="active">1</div>
 				<div>2</div>
 				<div>3</div>
 			</div>
-
-			<p className="message">Ello</p>
+			<p className="message">
+				Step {step}: {messages[step - 1]}
+			</p>
 			<div className="buttons">
-				<button>Previous</button>
-				<button>Next</button>
+				<button style={{backgroundColor: "#7950f2", color: "#fff"}}>
+					Previous
+				</button>
+				<button style={{backgroundColor: "#7950f2", color: "#fff"}}>
+					Next
+				</button>
 			</div>
 		</div>
 	);
