10de419aa0c6e1e791cf34e111abbb509d974b2e
Alan Webb <webbalan91939@gmail.com>
2024-10-23 03:30:01 +0100

    Update Counter component

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import "./index.css";
 
 export default function App() {
@@ -9,5 +10,31 @@ export default function App() {
 }
 
 function Counter() {
-	return <div>TEST</div>;
+	const [count, setCount] = useState(0);
+
+	const date = new Date();
+	date.setDate(date.getDate() + count);
+
+	return (
+		<>
+			<div>
+				<div>
+					<button onClick={() => setCount((c) => c - 1)}>-</button>
+					<span>Count: {count}</span>
+					<button onClick={() => setCount((c) => c + 1)}>+</button>
+				</div>
+			</div>
+
+			<p>
+				<span>
+					{count === 0
+						? "Today is "
+						: count > 0
+						? `${count} days from today is `
+						: `${Math.abs(count)} days ago was `}
+				</span>
+				<span>{date.toDateString()}</span>
+			</p>
+		</>
+	);
 }
