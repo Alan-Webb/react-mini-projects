92e7008922b1e0932e983339cc65799c213f4b9e
Alan Webb <webbalan91939@gmail.com>
2024-10-27 01:30:14 +0100

    Update BillInput component

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -20,7 +20,14 @@ function TipCalculator() {
 	);
 }
 
-function BillInput() {}
+function BillInput() {
+	return (
+		<div>
+			<label>How muich was the bill?</label>
+			<input type="text" placeholder="Bill value" />
+		</div>
+	);
+}
 
 function SelectPercentage() {}
 
