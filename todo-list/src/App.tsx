6fbee0c4bed0fbfda5c8a666820a93346836946b
Alan Webb <webbalan91939@gmail.com>
2024-11-14 00:43:37 +0000

    Update footer component styling

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -4,14 +4,20 @@ function App() {
 			<h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2 tracking-[0.2em]">
 				TodoApp
 			</h1>
-			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr]">
-				<header className="col-[1/3] row-[1/2] bg-[#fbf5ed]"></header>
+			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
+				<header className="col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]"></header>
 
 				<ul></ul>
-				<section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l"></section>
+
+				<section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]"></section>
 			</main>
 
-			<footer></footer>
+			<footer className="flex justify-between w-[972px] items-center text-[11px] opacity-30 mt-[12px]">
+				<small className="text-[11px]">&copy; 2024 Alan Webb</small>
+				<p>
+					Version <b>1.5</b>
+				</p>
+			</footer>
 		</div>
 	);
 }
