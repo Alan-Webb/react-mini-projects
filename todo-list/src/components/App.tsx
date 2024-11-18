5ef8131c494663eaefa2ba1eddf6b420e6c004c8
Alan Webb <webbalan91939@gmail.com>
2024-11-18 23:32:59 +0000

    Update Sidebar component

--- todo-list/src/components/App.tsx
+++ todo-list/src/components/App.tsx
@@ -1,6 +1,7 @@
 import BackgroundHeading from "./BackgroundHeading";
 import Header from "./Header";
 import TodoList from "./TodoList";
+import Sidebar from "./Sidebar";
 // import Footer from "./Footer";
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
 			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
 				<Header />
 				<TodoList />
-				<section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]"></section>
+				<Sidebar />
 			</main>
 			{/* <Footer /> */}
 		</div>
