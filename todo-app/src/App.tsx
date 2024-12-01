a49ec681cd684c85c3da6b733cb311b68823b4d8
Alan Webb <webbalan91939@gmail.com>
2024-12-01 01:16:44 +0000

    Update App component

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,11 +1,11 @@
 export default function App() {
 	return (
 		<div className="flex justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
-			<h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2">
+			<h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2 tracking-[0.2em]">
 				TodoApp
 			</h1>
 
-			<main className="w-[972px] h-[636px] bg-white">
+			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr]">
 				<header></header>
 
 				<ul></ul>
