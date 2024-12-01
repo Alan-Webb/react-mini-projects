adfaab10f7474a94fed4ca7b2f1d19c14df28807
Alan Webb <webbalan91939@gmail.com>
2024-12-01 19:35:40 +0000

    Update styling App component

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -5,12 +5,12 @@ export default function App() {
 				TodoApp
 			</h1>
 
-			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr]">
-				<header></header>
+			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr]">
+				<header className="col-[1/3] row-[1/2] bg-[#fbf5ed]"></header>
 
 				<ul></ul>
 
-				<section></section>
+				<section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]"></section>
 			</main>
 
 			<footer></footer>
