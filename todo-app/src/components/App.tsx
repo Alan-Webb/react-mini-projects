52fb1363b22f2921d454219f739609f03e8ceaeb
Alan Webb <webbalan91939@gmail.com>
2024-12-01 21:36:52 +0000

    Create Header component

--- todo-app/src/components/App.tsx
+++ todo-app/src/components/App.tsx
@@ -1,12 +1,15 @@
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
+import Header from "./Header";
+
 export default function App() {
 	return (
 		<div className="flex flex-col justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
 			<BackgroundHeading />
 
 			<main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
-				<header className="col-[1/3] row-[1/2] bg-[#fbf5ed]"></header>
+				<Header/>
+				
 
 				<ul></ul>
 
