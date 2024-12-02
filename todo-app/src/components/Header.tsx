48eccf09babfdf36f31b9d1052a5625f9ccd3330
Alan Webb <webbalan91939@gmail.com>
2024-12-02 03:36:39 +0000

    Update Header componenet

--- todo-app/src/components/Header.tsx
+++ todo-app/src/components/Header.tsx
@@ -1,3 +1,11 @@
 export default function Header() {
-	return <header className="col-[1/3] row-[1/2] bg-[#fbf5ed]"></header>;
+	return (
+		<header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed]">
+			<img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" />
+
+			<p>
+				<b>0</b> / 0 todos completed
+			</p>
+		</header>
+	);
 }
