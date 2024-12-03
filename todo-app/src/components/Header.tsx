7298d4272100dd18004ef6aa36a6db9e5e529f2d
Alan Webb <webbalan91939@gmail.com>
2024-12-03 06:10:51 +0000

    Create Counter component

--- todo-app/src/components/Header.tsx
+++ todo-app/src/components/Header.tsx
@@ -1,13 +1,12 @@
 import Logo from "./Logo";
+import Counter from "./Counter";
 
 export default function Header() {
 	return (
 		<header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed]">
 			<Logo />
 
-			<p>
-				<b>0</b> / 0 todos completed
-			</p>
+			<Counter />
 		</header>
 	);
 }
