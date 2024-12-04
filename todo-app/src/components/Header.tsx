d6d4001913640c0d0a8a1a1ce0542f56c7f516a1
Alan Webb <webbalan91939@gmail.com>
2024-12-04 13:19:53 +0000

    Update Header component styling

--- todo-app/src/components/Header.tsx
+++ todo-app/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Counter from "./Counter";
 
 export default function Header() {
 	return (
-		<header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed]">
+		<header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]">
 			<Logo />
 
 			<Counter />
