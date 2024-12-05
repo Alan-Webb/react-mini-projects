14edb77e861322cfc8d2b733343fbb5e580177ba
Alan Webb <webbalan91939@gmail.com>
2024-12-05 14:11:52 +0000

    Create AddTodoForm component

---
+++ todo-app/src/components/AddTodoForm.tsx
@@ -0,0 +1,9 @@
+export default function AddTodoForm() {
+	return (
+		<form>
+			<h2 className="font-medium text-[16px]">Add a Todo</h2>
+			<input type="text" />
+			<button type="submit">Add to list</button>
+		</form>
+	);
+}
--- todo-app/src/components/SideBar.tsx
+++ todo-app/src/components/SideBar.tsx
@@ -1,5 +1,8 @@
+import AddTodoForm from "./AddTodoForm"
 export default function SideBar() {
 	return (
-		<section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]"></section>
+		<section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]">
+			<AddTodoForm/>
+		</section>
 	);
 }
