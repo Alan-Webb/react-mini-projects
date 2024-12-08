6b3936c879263c422265c9710dbcc1045cd89f06
Alan Webb <webbalan91939@gmail.com>
2024-12-08 08:25:28 +0000

    Update AddTodoForm styling

--- todo-app/src/components/AddTodoForm.tsx
+++ todo-app/src/components/AddTodoForm.tsx
@@ -4,7 +4,7 @@ export default function AddTodoForm() {
 			<h2 className="font-medium text-[16px] text-[#231d15]">Add a Todo</h2>
 			<input
 				type="text"
-				className="h-[45px] border border-black/[12] rounded-[5px] my-[9px] text-[14px]"
+				className="h-[45px] border border-black/[12] rounded-[5px] my-[9px] text-[14px] block w-full"
 			/>
 			<button type="submit">Add to list</button>
 		</form>
