c8362c53ecc89001fc34cece459c13323ff245e1
Alan Webb <webbalan91939@gmail.com>
2024-12-10 08:45:56 +0000

    Create Button component

--- todo-app/src/components/AddTodoForm.tsx
+++ todo-app/src/components/AddTodoForm.tsx
@@ -4,9 +4,11 @@ export default function AddTodoForm() {
 			<h2 className="font-medium text-[16px] text-[#231d15]">Add a Todo</h2>
 			<input
 				type="text"
-				className="h-[45px] border border-black/[12] rounded-[5px] my-[9px] text-[14px] block w-full"
+				className="h-[45px] border border-black/[12] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]"
 			/>
-			<button type="submit" className="h-[45px] bg-[#473a2b] w-full text-white">
+			<button
+				type="submit"
+				className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
 				Add to list
 			</button>
 		</form>
---
+++ todo-app/src/components/Button.tsx
@@ -0,0 +1,9 @@
+export default function Button() {
+	return (
+		<button
+			type="submit"
+			className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
+			Add to list
+		</button>
+	);
+}
