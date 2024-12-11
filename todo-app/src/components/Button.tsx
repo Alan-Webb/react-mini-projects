8292f9e4f6e0a4bde85f15b1ecd6c7292a83470c
Alan Webb <webbalan91939@gmail.com>
2024-12-11 11:04:00 +0000

    Update AddTodoForm & Button components

--- todo-app/src/components/Button.tsx
+++ todo-app/src/components/Button.tsx
@@ -1,8 +1,6 @@
 export default function Button() {
 	return (
-		<button
-			type="submit"
-			className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
+		<button className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
 			Add to list
 		</button>
 	);
