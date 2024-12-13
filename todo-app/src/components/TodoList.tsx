2aebd84a93b97b20777bb4cf1ac096ae8e540f72
Alan Webb <webbalan91939@gmail.com>
2024-12-13 20:59:45 +0000

    Update TodoList component

--- todo-app/src/components/TodoList.tsx
+++ todo-app/src/components/TodoList.tsx
@@ -1,3 +1,14 @@
+const initialTodos = ["buy groceries", "Walk the dog", "Do laundry"];
+
 export default function TodoList() {
-	return <ul></ul>;
+	return (
+		<ul>
+			{initialTodos.map((todo) => (
+				<li className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]">
+					<span>{todo}</span>
+					<button>❌</button>
+				</li>
+			))}
+		</ul>
+	);
 }
