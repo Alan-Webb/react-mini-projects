d8f441818c3d0f6e3e1dc5cb9fd79ee7e4d7c2a4
Alan Webb <webbalan91939@gmail.com>
2024-12-20 19:48:45 +0000

    Update TodoList component

--- todo-app/src/components/TodoList.tsx
+++ todo-app/src/components/TodoList.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import DeleteButton from "./DeleteButton";
+// import DeleteButton from "./DeleteButton";
 
 export default function TodoList() {
 	const [todos, setTodos] = useState([
@@ -13,22 +13,17 @@ export default function TodoList() {
 			{todos.map((todo) => (
 				<li
 					key={todo.id}
-					className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
-					onClick={() => {
-						setTodos(
-							todos.map((todo) => {
-								return {
-									...todo,
-									isCompleted: true,
-								};
-							})
-						);
-					}}>
+					className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]">
 					<span
 						className={`${todo.isCompleted ? "line-through text-[#ccc]" : ""}`}>
 						{todo.text}
 					</span>
-					<button>❌</button>
+					<button
+						onClick={() => {
+							setTodos([]);
+						}}>
+						❌
+					</button>
 				</li>
 			))}
 		</ul>
