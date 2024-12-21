780bab22dea6add6da38ab79af3297a2cc9b9147
Alan Webb <webbalan91939@gmail.com>
2024-12-21 20:02:12 +0000

    Update Header & Logo components

--- todo-app/src/components/Header.tsx
+++ todo-app/src/components/Header.tsx
@@ -1,9 +1,9 @@
-import Logo from "./Logo";
 import Counter from "./Counter";
+import Logo from "./Logo";
 
 export default function Header() {
 	return (
-		<header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]">
+		<header className="col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-b-[rgba(0,0,0,0.04)] flex justify-between items-center px-[28px] py-[0]">
 			<Logo />
 
 			<Counter />
