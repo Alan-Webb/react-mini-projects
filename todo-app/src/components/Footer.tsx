b4dac0b4431bcd0d90039a0fb90bc3619a10d457
Alan Webb <webbalan91939@gmail.com>
2024-12-21 20:01:30 +0000

    Update Counter & Footer components

--- todo-app/src/components/Footer.tsx
+++ todo-app/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
-		<footer className="flex justify-between w-[972px] items-center text-[11px] opacity-30 mt-[12px]">
-			<small className="text-[11px]">&copy; 2025. Copyright AW Dev</small>
+		<footer className="w-[972px] flex justify-between items-center text-[11px] opacity-30 mt-[12px]">
+			<small className="text-[11px]">&copy; 2050. Copyright by ByteGrad.</small>
 			<p>
 				Version <b>1.5</b>
 			</p>
