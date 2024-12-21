9b0908f4a226042409b31e72cd85b6635d67c57f
Alan Webb <webbalan91939@gmail.com>
2024-12-21 20:00:08 +0000

    Update App & AddTodoForm components

--- todo-app/src/components/AddTodoForm.tsx
+++ todo-app/src/components/AddTodoForm.tsx
@@ -1,12 +1,27 @@
+import {useState} from "react";
+import {useTodosContext} from "../lib/hooks";
 import Button from "./Button";
 
 export default function AddTodoForm() {
+	const [todoContent, setTodoContent] = useState("");
+	const {addTodo} = useTodosContext();
+
 	return (
-		<form>
-			<h2 className="font-medium text-[16px] text-[#231d15]">Add a Todo</h2>
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+				addTodo(todoContent);
+				setTodoContent("");
+			}}>
+			<h2 className="text-[#231d15] text-[16px] font-medium">Add a todo</h2>
 			<input
 				type="text"
-				className="h-[45px] border border-black/[12] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]"
+				autoFocus
+				className="h-[45px] w-full border-[1px] border-[rgba(0,0,0,0.12)] rounded-[5px] ml-[0] mr-[0] my-[9px] px-[16px] py-[0] text-[14px]"
+				value={todoContent}
+				onChange={(e) => {
+					setTodoContent(e.target.value);
+				}}
 			/>
 			<Button>Add to list</Button>
 		</form>
