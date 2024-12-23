5406dc3645b97f0cf2187e99455ba05ab34adcaa
Alan Webb <webbalan91939@gmail.com>
2024-12-23 00:24:42 +0000

    Update App component

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -1,5 +1,38 @@
 import "./App.css";
+import {useState} from "react";
 
 export default function App() {
-	return <></>;
+	const [time, setTime] = useState(0);
+	const [Running, setRunning] = useState(false);
+
+	return (
+		<>
+			<h1>Stopwatch</h1>
+			<div>
+				<span>{"0" + Math.floor((time / 60000) % 60)}:</span>
+				<span>{"0" + Math.floor((time / 1000) % 60)}:</span>
+				<span>{"0" + ((time / 10) % 60)}</span>
+			</div>
+			<div>
+				<button
+					onClick={() => {
+						setRunning(true);
+					}}>
+					Start
+				</button>
+				<button
+					onClick={() => {
+						setRunning(false);
+					}}>
+					Stop
+				</button>
+				<button
+					onClick={() => {
+						setTime(0);
+					}}>
+					Reset
+				</button>
+			</div>
+		</>
+	);
 }
