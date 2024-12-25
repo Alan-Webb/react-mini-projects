90b8359c644e9c2437836b4445f027f65c3ae0ee
Alan Webb <webbalan91939@gmail.com>
2024-12-25 16:41:57 +0000

    Update App component

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -18,16 +18,17 @@ export default function App() {
 	}, [running]);
 
 	return (
-		<div className="max-w-md flex flex-col items-center justify-center py-8">
-			<h1 className="text-2xl font-semibold pb-2">Stopwatch</h1>
-			<div className="text-xl font-semibold">
+		<div className="flex flex-col items-center justify-center py-12">
+			<h1 className="text-2xl font-semibold">Stopwatch</h1>
+			<div className="text-xl font-semibold py-4">
 				<span>{"0" + Math.floor((time / 60000) % 60)}:</span>
 				<span>{"0" + Math.floor((time / 1000) % 60)}:</span>
 				<span>{"0" + ((time / 10) % 100)}</span>
 			</div>
-			<div>
+			<div className="w=1/3 flex flex-row justify-between">
 				{running ? (
 					<button
+						className="border rounded-lg py-1 px-2.5 me-2"
 						onClick={() => {
 							setRunning(false);
 						}}>
@@ -35,6 +36,7 @@ export default function App() {
 					</button>
 				) : (
 					<button
+						className="border rounded-lg py-1 px-3 me-2"
 						onClick={() => {
 							setRunning(true);
 						}}>
@@ -43,6 +45,7 @@ export default function App() {
 				)}
 
 				<button
+					className="border rounded-lg py-1 px-2.5 ms-2"
 					onClick={() => {
 						setTime(0);
 					}}>
