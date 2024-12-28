dcf252709d4ac22dd6f576e0b51b362ad1c8f228
Alan Webb <webbalan91939@gmail.com>
2024-12-28 20:58:39 +0000

    Create Board component

--- todo-board/src/App.jsx
+++ todo-board/src/App.jsx
@@ -1,18 +1,21 @@
 import {useState} from "react";
 import Input from "./components/Input";
+import Board from "./components/Board";
 
 export default function App() {
 	const [taskList, setTaskList] = useState([]);
 
 	return (
-		<div className="flex flex-col items-center justify-center py-8 gap-4">
-			<h1 className="text-xl font-semibold">Todo Board</h1>
-			<Input taskList={taskList} setTaskList={setTaskList} />
+		<>
+			<div className="flex flex-col items-center justify-center py-8 gap-4">
+				<h1 className="text-xl font-semibold">Todo Board</h1>
+				<Input taskList={taskList} setTaskList={setTaskList} />
+			</div>
 			<div>
 				{taskList.map((task, index) => (
-					<li key={index}>{task}</li>
+					<Board/>>
 				))}
 			</div>
-		</div>
+		</>
 	);
 }
