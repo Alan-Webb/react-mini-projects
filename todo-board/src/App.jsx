5264e1e7a59263b9ce0822e3a64368edff22780e
Alan Webb <webbalan91939@gmail.com>
2024-12-28 20:54:16 +0000

    Update Input & App components

--- todo-board/src/App.jsx
+++ todo-board/src/App.jsx
@@ -5,8 +5,8 @@ export default function App() {
 	const [taskList, setTaskList] = useState([]);
 
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<h1 className="font-semibold">Todo Board</h1>
+		<div className="flex flex-col items-center justify-center py-8 gap-4">
+			<h1 className="text-xl font-semibold">Todo Board</h1>
 			<Input taskList={taskList} setTaskList={setTaskList} />
 			<div>
 				{taskList.map((task, index) => (
