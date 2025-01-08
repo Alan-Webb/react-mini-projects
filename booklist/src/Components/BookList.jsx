a7e56725f6b0d337366577c6b1eeea90c060d4bb
Alan Webb <webbalan91939@gmail.com>
2025-01-08 02:01:43 +0000

    Create Books component

--- booklist/src/Components/BookList.jsx
+++ booklist/src/Components/BookList.jsx
@@ -1,20 +1,5 @@
 import Book from "./Book";
-
-const books = [
-	{
-		author: "Max Brooks",
-		title: "The Zombie Survival Handbook",
-		img: "./src/images/book-1.jpg",
-		id: 1,
-	},
-
-	{
-		author: "Sun Tzu",
-		title: "The Art of War",
-		img: "./src/images/book-2.jpg",
-		id: 2,
-	},
-];
+import {books} from "./Books";
 
 export default function BookList() {
 	return (
