f5baabad2d366aa81972a54edc6bdfb51eec1425
Alan Webb <webbalan91939@gmail.com>
2025-01-09 00:05:05 +0000

    Create major components & add assets

--- backroads-app/src/App.jsx
+++ backroads-app/src/App.jsx
@@ -1,5 +1,20 @@
-import "./App.css";
+import "./index.css";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+import Footer from "./Components/Footer";
 
 export default function App() {
-	return <h1>Backroads App</h1>;
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<About />
+			<Services />
+			<Tours />
+			<Footer />
+		</>
+	);
 }
