3c644809a43f386aa813a0b30ebbc05d190cc35a
Alan Webb <webbalan91939@gmail.com>
2025-02-08 05:44:11 +0000

    Reorganize files

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import TodoList from "./components/TodoList/TodoList";
-import TodoItem from "./components/TodoList/TodoItem/TodoItem";
 
 export default function App() {
-	return (
-		<>
-			<TodoList></TodoList>
-		</>
-	);
+	return <></>;
 }
