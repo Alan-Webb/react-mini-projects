dfd9614e68db50429b6ebdac24859225d36f221a
Alan Webb <webbalan91939@gmail.com>
2025-02-09 22:36:17 +0000

    Update TaskForm component

--- To-Do-App/src/components/TaskForm.jsx
+++ To-Do-App/src/components/TaskForm.jsx
@@ -1,4 +1,21 @@
+import React from "react";
+
 const TaskForm = () => {
-	return <div>TaskForm</div>;
+	return (
+		<form>
+			<input type="text" placeholder="Task Description" required />
+			<input type="date" required />
+			<select>
+				<option value="low">Low</option>
+				<option value="medium">Medium</option>
+				<option value="high">High</option>
+			</select>
+			<label>
+				Completed:
+				<input type="checkbox" />
+			</label>
+			<button type="submit">Add Task</button>
+		</form>
+	);
 };
 export default TaskForm;
