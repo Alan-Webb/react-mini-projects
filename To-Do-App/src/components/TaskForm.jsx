9f8e7dc3b5bc9353329ac2e3dae4040dd072ad51
Alan Webb <webbalan91939@gmail.com>
2025-02-10 19:23:04 +0000

    Update TaskForm component

--- To-Do-App/src/components/TaskForm.jsx
+++ To-Do-App/src/components/TaskForm.jsx
@@ -1,28 +1,51 @@
 import {useState} from "react";
 
 const TaskForm = () => {
-	const [description, setDescription] = useState("Random Task");
+	const [description, setDescription] = useState("");
+	const [deadline, setDeadline] = useState("");
+	const [priority, setPriority] = useState("low");
+	const [completed, setCompleted] = useState(false);
+
+	const handelSubmit = (e) => {
+		e.preventDefault();
+		const newTask = {
+			id: Date.now(),
+			description,
+			deadline,
+			priority,
+			completed,
+		};
+	};
 
 	return (
-		<form>
+		<form onSubmit={handelSubmit}>
 			<input
-				type="text"
-				placeholder="Task Description"
-				required
 				value={description}
 				onChange={(e) => {
 					setDescription(e.target.value);
 				}}
+				type="text"
+				placeholder="Task Description"
+				required
 			/>
-			<input type="date" required />
-			<select>
+			<input
+				value={deadline}
+				onChange={(e) => setDeadline(e.target.value)}
+				type="date"
+				required
+			/>
+			<select value={priority} onChange={(e) => setPriority(e.target.value)}>
 				<option value="low">Low</option>
 				<option value="medium">Medium</option>
 				<option value="high">High</option>
 			</select>
 			<label>
 				Completed:
-				<input type="checkbox" />
+				<input
+					value={completed}
+					onChange={(e) => setCompleted(e.target.completed)}
+					type="checkbox"
+				/>
 			</label>
 			<button type="submit">Add Task</button>
 		</form>
