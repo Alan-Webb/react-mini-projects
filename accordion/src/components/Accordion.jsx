df00f1ef1a2c47a8cb3a8325da13bdc973ab6fe9
Alan Webb <webbalan91939@gmail.com>
2025-02-13 17:54:49 +0000

    Update Accordion component

--- accordion/src/components/Accordion.jsx
+++ accordion/src/components/Accordion.jsx
@@ -1,16 +1,29 @@
 import {useState} from "react";
+import data from "./data";
+import "./styles.css";
 
 const Accordion = () => {
 	const [selected, setSelected] = useState(null);
 
+	function handleSingleSelection(getCurrentId) {}
+
 	return (
-		<div className="wrapper">
+		<div className="acc-wrapper">
 			<div className="accordion">
-				{
-					data && data.length > 0 ?
-					data.map(dataItem => <div className="item"></div>)
-					: <div>No data found</div>
-				}
+				{data && data.length > 0 ? (
+					data.map((dataItem) => (
+						<div className="item">
+							<div
+								onClick={() => handleSingleSelection(dataItem.id)}
+								className="title">
+								<h3>{dataItem.question}</h3>
+								<spam>+</spam>
+							</div>
+						</div>
+					))
+				) : (
+					<div>No data found !</div>
+				)}
 			</div>
 		</div>
 	);
