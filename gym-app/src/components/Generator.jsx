6ab73fd9029a39b24f2771dfb9f1509103ab098f
Alan Webb <webbalan91939@gmail.com>
2025-02-18 00:36:59 +0000

    Update Generator component

--- gym-app/src/components/Generator.jsx
+++ gym-app/src/components/Generator.jsx
@@ -1,12 +1,29 @@
 import SectionWrapper from "./SectionWrapper";
 
+function Header(props) {
+	const {index, title, description} = props;
+	return (
+		<div>
+			<div>
+				<p>{index}</p>
+				<h4>{title}</h4>
+			</div>
+			<p>{description}</p>
+		</div>
+	);
+}
+
 export default function Generator() {
 	return (
 		<div className="min-h-screen">
 			<SectionWrapper
 				header={"generate your workout"}
 				title={["It's", " shredded", " O'clock"]}>
-				info
+				<Header
+					index={"01"}
+					title={"Define your goals"}
+					description={"Select your workout"}
+				/>
 			</SectionWrapper>
 		</div>
 	);
