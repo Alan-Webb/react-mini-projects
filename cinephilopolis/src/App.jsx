5d2af05f5218d6a7fd9a1371fb106a29a0756eb3
Alan Webb <webbalan91939@gmail.com>
2025-02-22 19:45:28 +0000

    Update Search component

--- cinephilopolis/src/App.jsx
+++ cinephilopolis/src/App.jsx
@@ -1,15 +1,22 @@
+import {useState} from "react";
+import Search from "./components/Search.jsx";
+
 function App() {
+	const [searchTerm, setSearchTerm] = useState("");
+
 	return (
 		<main>
 			<div className="pattern" />
 			<div className="wrapper">
 				<header>
+					<img src="./src/assets/hero.png" alt="movie posters" />
 					<h1 className="uppercase">
 						Find the <span className="text-gradient">movies</span> you want at
 						<span className="text-gradient"> cinephilopolis</span>
 					</h1>
 				</header>
-        <p>Search</p>
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+				<h1 className="text-white">{searchTerm}</h1>
 			</div>
 		</main>
 	);
--- cinephilopolis/src/components/Search.jsx
+++ cinephilopolis/src/components/Search.jsx
@@ -1,4 +1,17 @@
-const Search = () => {
-	return <div>Search</div>;
+const Search = ({searchTerm, setSearchTerm}) => {
+	return (
+		<div className="search">
+			<div>
+				<img src="./src/assets/search.svg" alt="search" />
+				<input
+					type="text"
+					placeholder="Search through thousands of movies"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+				/>
+			</div>
+		</div>
+	);
 };
+
 export default Search;
