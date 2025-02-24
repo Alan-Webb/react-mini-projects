c13c1795cfca4cda937d7b7cd60b0de87ea524de
Alan Webb <webbalan91939@gmail.com>
2025-02-24 00:22:13 +0000

    Create and update Workflow component

--- vr-landing-page/src/App.jsx
+++ vr-landing-page/src/App.jsx
@@ -1,3 +1,4 @@
+import Workflow from "./components/Workflow";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -9,6 +10,7 @@ const App = () => {
 			<div className="max-w-7xl mx-auto pt-20 px-6">
 				<Hero />
 				<Features />
+				<Workflow />
 			</div>
 		</div>
 	);
