3520a6b0bb945940b09f1e5a4942c66e2ec12c5b
Alan Webb <webbalan91939@gmail.com>
2025-02-26 00:09:28 +0000

    Add animations to the landing page

--- restaura-landing-page/src/components/Expertise.jsx
+++ restaura-landing-page/src/components/Expertise.jsx
@@ -1,4 +1,26 @@
 import {CUISINES} from "../constants";
+import {motion} from "framer-motion";
+
+const containerVariants = {
+	hidden: {
+		opacity: 0,
+	},
+	show: {
+		opacity: 1,
+	},
+	transition: {
+		staggerChildren: 1,
+	},
+};
+
+const itemVariants = {
+	hidden: {opacity: 0, y: 20},
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: {duration: 0.8},
+	},
+};
 
 const Expertise = () => {
 	return (
@@ -6,9 +28,14 @@ const Expertise = () => {
 			<h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl ">
 				Our Expertise
 			</h2>
-			<div className="container mx-auto px-4">
+			<motion.div
+				initial="hidden"
+				whileInView="show"
+				variants={containerVariants}
+				className="container mx-auto px-4">
 				{CUISINES.map((cuisine, index) => (
-					<div
+					<motion.div
+						variants={itemVariants}
 						key={index}
 						className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2">
 						<div className="flex-shrink-0 pr-8 text-2xl">{cuisine.number}</div>
@@ -27,9 +54,9 @@ const Expertise = () => {
 								{cuisine.description}
 							</p>
 						</div>
-					</div>
+					</motion.div>
 				))}
-			</div>
+			</motion.div>
 		</section>
 	);
 };
--- restaura-landing-page/src/components/Mission.jsx
+++ restaura-landing-page/src/components/Mission.jsx
@@ -1,6 +1,7 @@
 import missionImg from "../assets/mission.jpeg";
 import mission from "../assets/mission.mp4";
-import { MISSION } from "../constants";
+import {MISSION} from "../constants";
+import {motion} from "framer-motion";
 
 const Mission = () => {
 	return (
@@ -8,19 +9,33 @@ const Mission = () => {
 			<div className="container mx-auto text-center">
 				<h2 className="mb-8 text-3xl lg:text-4xl">Our Mission</h2>
 				<div className="relative flex items-center justify-center">
-					<video
+					<motion.video
 						className="w-full rounded-3xl"
+						initial={{opacity: 0}}
+						whileInView={{opacity: 1}}
+						viewport={{once: true}}
+						transition={{duration: 1}}
 						autoPlay
 						loop
 						muted
 						playsInline
 						poster={missionImg}>
 						<source src={mission} type="video/mp4" />
-					</video>
-          <div className="absolute h-full w-full rounded-3xl bg-black/40"></div>
-          <p className="absolute max-w-lg tracking-tighter lg:text-3xl">
-            {MISSION}
-          </p>
+					</motion.video>
+					<motion.div
+						className="absolute h-full w-full rounded-3xl bg-black/40"
+						initial={{opacity: 0}}
+						whileInView={{opacity: 1}}
+						viewport={{once: true}}
+						transition={{duration: 0.5, delay: 0.5}}></motion.div>
+					<motion.p
+						className="absolute max-w-lg tracking-tighter lg:text-3xl"
+						initial={{opacity: 0, y: 20}}
+						whileInView={{opacity: 1, y: 0}}
+						viewport={{once: true}}
+						transition={{duration: 1, delay: 0.5}}>
+						{MISSION}
+					</motion.p>
 				</div>
 			</div>
 		</section>
