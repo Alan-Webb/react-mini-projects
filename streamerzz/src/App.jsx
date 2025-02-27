a42643c4a92946656c9d29de55caaa33b7c5a7ac
Alan Webb <webbalan91939@gmail.com>
2025-02-27 23:52:15 +0000

    Create HowItWorks component

--- streamerzz/src/App.jsx
+++ streamerzz/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
 	return (
 		<main className="text-sm text-neutral-300 antialiased">
 			<Navbar />
 			<Hero />
+			<HowItWorks />
 		</main>
 	);
 };
---
+++ streamerzz/src/components/HowItWorks.jsx
@@ -0,0 +1,18 @@
+import {HOW_IT_WORKS_CONTENT} from "../constants";
+
+const HowItWorks = () => {
+	return (
+		<section id="works">
+			<div className="max-w-7xl mx-auto px-4">
+				<div className="text-center mb-12 border-t border-neutral-800">
+					<h2 className="text-3xl lg:text-5xl mt-20 tracking-tighter bg-gradient-to-t from-neutral-50 via-neutral-300 to-neutral-600 bg-clip-text text-transparent">
+						{HOW_IT_WORKS_CONTENT.sectionTitle}
+					</h2>
+          <p className=""></p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default HowItWorks;
