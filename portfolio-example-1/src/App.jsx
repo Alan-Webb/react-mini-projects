fd78ba62c8ea9649eb0a28a00034abcc29e418b9
Alan Webb <webbalan91939@gmail.com>
2025-03-03 00:43:49 +0000

    Create & update Hero component

--- portfolio-example-1/src/App.jsx
+++ portfolio-example-1/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
 	return (
 		<main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
 			<Navbar />
+			<Hero />
 		</main>
 	);
 };
