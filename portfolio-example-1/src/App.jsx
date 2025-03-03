718dd2da4ff4e01f537544bd014b685e84cbecbf
Alan Webb <webbalan91939@gmail.com>
2025-03-03 00:53:37 +0000

    Create & update About component

--- portfolio-example-1/src/App.jsx
+++ portfolio-example-1/src/App.jsx
@@ -1,11 +1,13 @@
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
 	return (
 		<main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
 			<Navbar />
 			<Hero />
+			<About />
 		</main>
 	);
 };
