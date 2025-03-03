da2792a019b5ef3ba3494fecede214e5874ab34e
Alan Webb <webbalan91939@gmail.com>
2025-03-03 01:04:21 +0000

    Update Projects & Card components

--- portfolio-example-1/src/components/Projects.jsx
+++ portfolio-example-1/src/components/Projects.jsx
@@ -1,7 +1,24 @@
+import {PROJECTS} from "../constants/index.jsx";
 import Card from "./Card.jsx";
 
 const Projects = () => {
-	return <div>Projects</div>;
+	return (
+		<div id="projects">
+			<h2 className="mt-20 text-center text-4xl font-semibold">Projects</h2>
+			<div className="flex flex-wrap justify-center py-8">
+				{PROJECTS.map((project, index) => (
+					<div key={index}>
+						<Card
+							image={project.image}
+							title={project.title}
+							subtitle={project.subtitle}
+							link="#"
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
