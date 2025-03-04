db7c90299bb76c81ca86c32ce30b9dcc54dec923
Alan Webb <webbalan91939@gmail.com>
2025-03-04 19:28:26 +0000

    Add animation to About, Hero & Projects components

--- portfolio-example-1/src/components/Hero.jsx
+++ portfolio-example-1/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import {PROFILE} from "../constants";
 import profilePic from "../assets/robertButcher1.jpg";
+import {motion} from "framer-motion";
 
 const Hero = () => {
 	return (
@@ -7,18 +8,29 @@ const Hero = () => {
 			<div
 				className="relative flex min-h-screen items-end justify-center"
 				id="hero">
-				<img
+				<motion.img
+					initial={{opacity: 0}}
+					animate={{opacity: 1}}
+					transition={{duration: 1}}
 					src={profilePic}
 					alt={PROFILE.name}
 					className="absolute inset-0 z-10 h-full w-full object-cover"
 				/>
-				<div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent from-60% to-black lg:from-30%"></div>
-				<div className="z-20 mx-4 max-w-3xl pb-20">
+				<motion.div
+					initial={{opacity: 0}}
+					animate={{opacity: 1}}
+					transition={{duration: 1}}
+					className="absolute inset-0 z-10 bg-gradient-to-b from-transparent from-60% to-black lg:from-30%"></motion.div>
+				<motion.div
+					initial={{opacity: 0}}
+					animate={{opacity: 1}}
+					transition={{duration: 1, delay: 1}}
+					className="z-20 mx-4 max-w-3xl pb-20">
 					<h1 className="text-5xl font-semibold uppercase tracking-wide md:text-7xl">
 						{PROFILE.name}
 					</h1>
 					<p className="pt-2 font-semibold">{PROFILE.info}</p>
-				</div>
+				</motion.div>
 			</div>
 		</>
 	);
