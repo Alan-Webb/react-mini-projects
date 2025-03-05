7520be00d6d752409f99627fa12a5a4ae2a18ff8
Alan Webb <webbalan91939@gmail.com>
2025-03-05 00:46:53 +0000

    Update Navbar component

--- portfolio-example-2/src/App.jsx
+++ portfolio-example-2/src/App.jsx
@@ -4,9 +4,9 @@ const App = () => {
 	return (
 		<div className="overflow-x-hidden text-stone-300 antialiased">
 			<div className="fixed inset-0 -z-10">
-				<div class="relative h-full w-full bg-slate-950">
-					<div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-					<div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+				<div className="relative h-full w-full bg-black-950">
+					<div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+					<div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
 				</div>
 			</div>
 			<div className="container mx-auto px-8">
--- portfolio-example-2/src/components/Navbar.jsx
+++ portfolio-example-2/src/components/Navbar.jsx
@@ -1,7 +1,44 @@
+import logo from "../assets/raviKumarLogo.webp";
+import {FaGithub, FaInstagram, FaLinkedin, FaTwitter} from "react-icons/fa";
+
 const Navbar = () => {
 	return (
-		<nav>
-			<div></div>>
+		<nav className="flex items-center justify-between py-6">
+			<div className="flex flex-shrink-0 items-center">
+				<a href="/" aria-label="Home">
+					<img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
+				</a>
+			</div>
+			<div className="m-8 flex items-center justify-center gap-4 text-2xl">
+				<a
+					href="https://www.linkedin.com/in/your-linkedin-profile"
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label="LinkedIn">
+					<FaLinkedin />
+				</a>
+				<a
+					href="https://www.github.com/in/your-github-profile"
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label="GitHub">
+					<FaGithub />
+				</a>
+				<a
+					href="https://www.instagram.com/in/your-instagram-profile"
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label="Instagram">
+					<FaInstagram />
+				</a>
+				<a
+					href="https://www.twitter.com/in/your-twitter-profile"
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label="Twitter">
+					<FaTwitter />
+				</a>
+			</div>
 		</nav>
 	);
 };
