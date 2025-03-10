81b7e4110d6137404d09f846771b6dd0a5b99a85
Alan Webb <webbalan91939@gmail.com>
2025-03-10 19:54:52 +0000

    Update Technologies component

--- portfolio-example-2/src/components/Technologies.jsx
+++ portfolio-example-2/src/components/Technologies.jsx
@@ -1,6 +1,9 @@
 import {RiReactjsLine} from "react-icons/ri";
 import {TbBrandNextjs} from "react-icons/tb";
 import {SiMongodb} from "react-icons/si";
+import {DiRedis} from "react-icons/di";
+import {FaNodeJs} from "react-icons/fa";
+import {BiLogoPostgresql} from "react-icons/bi";
 
 const Technologies = () => {
 	return (
@@ -15,6 +18,15 @@ const Technologies = () => {
 				</div>
 				<div className="p-4">
 					<SiMongodb className="text-7xl text-cyan-500" />
+				</div>
+				<div className="p-4">
+					<DiRedis className="text-7xl text-red-700" />
+				</div>
+				<div className="p-4">
+					<FaNodeJs className="text-7xl text-green-500" />
+				</div>
+				<div className="p-4">
+					<BiLogoPostgresql className="text-7xl text-sky-700" />
 				</div>
 			</div>
 		</div>
