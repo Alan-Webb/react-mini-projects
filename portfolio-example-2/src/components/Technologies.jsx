de96267dc5dbf7d170593d746e79a93645ff8708
Alan Webb <webbalan91939@gmail.com>
2025-03-10 00:09:40 +0000

    Update Technologies component

--- portfolio-example-2/src/components/Technologies.jsx
+++ portfolio-example-2/src/components/Technologies.jsx
@@ -1,5 +1,24 @@
+import {RiReactjsLine} from "react-icons/ri";
+import {TbBrandNextjs} from "react-icons/tb";
+import {SiMongodb} from "react-icons/si";
+
 const Technologies = () => {
-	return <div>Technologies</div>;
+	return (
+		<div className="pb-24">
+			<h2 className="my-20 text-center text-4xl ">Technologies</h2>
+			<div className="flex flex-wrap items-center justify-center gap-4">
+				<div>
+					<RiReactjsLine className="text-7xl text-cyan-400" />
+				</div>
+				<div className="p-4">
+					<TbBrandNextjs className="text-7xl" />
+				</div>
+				<div className="p-4">
+					<SiMongodb className="text-7xl text-cyan-500" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Technologies;
