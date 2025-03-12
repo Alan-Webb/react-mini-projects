426e19ed109fa7c6d02d820ca34d44c9d16003b2
Alan Webb <webbalan91939@gmail.com>
2025-03-12 02:29:08 +0000

    Create & update About component

--- portfolio-example-3/src/App.jsx
+++ portfolio-example-3/src/App.jsx
@@ -2,6 +2,7 @@ import BlurBackground from "./components/BlurBackground";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import About from "./components/About";
 
 const App = () => {
 	return (
@@ -12,6 +13,7 @@ const App = () => {
 				<Navbar />
 				<Hero />
 				<Projects />
+				<About />
 			</main>
 		</>
 	);
