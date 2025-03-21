312edf6eade7cfbd1d1b54495173a2c038eb550c
Alan Webb <webbalan91939@gmail.com>
2025-03-21 19:11:47 +0000

    Update CSS, create & update Navbar component

--- portfolio-example/src/App.jsx
+++ portfolio-example/src/App.jsx
@@ -1,10 +1,10 @@
 const App = () => {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
+		<main className="font-light text-white antialiased selection:bg-linear-300
+		selection:text-black">
+		
+		</main>
 	);
 };
 
 export default App;
- 
