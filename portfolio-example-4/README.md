91d1bf9b3260f277f02b207444e2f608d9973dad
Alan Webb <webbalan91939@gmail.com>
2025-03-26 20:34:36 +0000

    Update Navbar, Hero & Marquee components

---
+++ portfolio-example-4/README.md
@@ -0,0 +1,12 @@
+# Portfolio-example-4
+
+Landing page created follow tutorial found at https://www.youtube.com/watch?v=UhRi2KbmUDA
+
+## Built with
+
+- React
+- TailwindCSS
+
+## Author
+
+Kushald
---
+++ portfolio-example-4/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Marquee from "./components/Marquee";
+
+const App = () => {
+	return (
+		<main className="font-light text-white antialiased selection:bg-lime-300 selection:text-black">
+			<Navbar />
+			<Hero />
+			<Marquee />
+		</main>
+	);
+};
+export default App;
