966ecfdba15a1214f83503ee669b0a1a1d9a9cb0
Alan Webb <webbalan91939@gmail.com>
2025-03-28 00:12:51 +0000

    Add animation to the Marquee component

--- portfolio-example-4/src/components/Marquee.jsx
+++ portfolio-example-4/src/components/Marquee.jsx
@@ -1,15 +1,19 @@
 import {MARQUEE_TEXT} from "../constants";
+import {motion} from "framer-motion";
 
 const Marquee = () => {
 	return (
 		<div className="mt-4 w-full bg-lime-300 text-black lg:py-6">
 			<div className="flex overflow-hidden whitespace-nowrap">
 				{[...Array(2)].map((_, i) => (
-					<h1
+					<motion.h1
+						initial={{x: "-100%"}}
+						animate={{x: "0"}}
+						transition={{repeat: Infinity, duration: 20, ease: "linear"}}
 						key={i}
 						className="py-2 text-3xl font-bold leading-none tracking-tighter lg:text-7xl">
 						{MARQUEE_TEXT}
-					</h1>
+					</motion.h1>
 				))}
 			</div>
 		</div>
@@ -17,4 +21,3 @@ const Marquee = () => {
 };
 
 export default Marquee;
- 
