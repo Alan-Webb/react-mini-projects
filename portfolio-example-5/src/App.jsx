bebbe45578aa496509c11df5d87d97ec2a3f6d94
Alan Webb <webbalan91939@gmail.com>
2025-03-31 06:05:46 +0100

    Create & update Bio component

--- portfolio-example-5/src/App.jsx
+++ portfolio-example-5/src/App.jsx
@@ -1,3 +1,4 @@
+import Bio from "./components/Bio";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -10,6 +11,7 @@ const App = () => {
 				<Hero />
 				<Navbar />
 				<Projects />
+				<Bio />
 			</div>
 		</div>
 	);
