6448e25c29e84034f2d4deb21aa84211e2f8b0b1
Alan Webb <webbalan91939@gmail.com>
2025-04-01 07:16:27 +0100

    Create Education component

--- portfolio-example-5/src/App.jsx
+++ portfolio-example-5/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
+import Education from "./components/Education";
 
 const App = () => {
 	return (
@@ -16,6 +17,7 @@ const App = () => {
 				<Bio />
 				<Skills />
 				<Experience />
+				<Education />
 			</div>
 		</div>
 	);
