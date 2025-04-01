75fdd5cca3b5b0a9877626ffe77ca0c9db13f3df
Alan Webb <webbalan91939@gmail.com>
2025-04-01 07:17:43 +0100

    Create a new component called Contact.jsx in the src/components directory. This compon

--- portfolio-example-5/src/App.jsx
+++ portfolio-example-5/src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
+import Contact from "./components/Contact";
 
 const App = () => {
 	return (
@@ -18,6 +19,7 @@ const App = () => {
 				<Skills />
 				<Experience />
 				<Education />
+				<Contact />
 			</div>
 		</div>
 	);
