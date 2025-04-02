4575bc756596a914d8c1ab81fc79a788855db310
Alan Webb <webbalan91939@gmail.com>
2025-04-03 00:00:59 +0100

    Update Contact.jsx

--- portfolio-example-5/src/components/Contact.jsx
+++ portfolio-example-5/src/components/Contact.jsx
@@ -1,4 +1,34 @@
+import emailjs from "emailjs/browser";
+import {useState} from "react";
+
 const Contact = () => {
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
+	const [error, setError] = useState({});
+	const [isSending, setIsSending] = useState(false);
+
+	const handleChange = (e) => {
+		const {name, value} = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const validate = () => {
+		let errors = {};
+		if (!formData.name) errors.name = "Name is required";
+		if (!formData.email) {
+			errors.name = "Email is required";
+		} else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+			errors.email = "Email is invalid";
+		}
+		if (!formData.message) errors.message = "Message is required";
+	};
 	return <div>Contact</div>;
 };
 
