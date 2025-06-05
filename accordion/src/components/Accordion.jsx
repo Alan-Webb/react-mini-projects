7d6a163a8f78a2ce12308315d627b2369f23539d
Alan Webb <webbalan91939@gmail.com>
2025-06-05 11:22:58 +0100

    Update mini-project

--- accordion/src/components/Accordion.jsx
+++ accordion/src/components/Accordion.jsx
@@ -12,7 +12,6 @@ const Accordion = () => {
 	}
 
 	function handleMultiSelection(getCurrentId) {
-		// Stage 6
 		let cpyMultiple = [...multiple];
 		const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
 		if (findIndexOfCurrentId === -1) {
@@ -20,9 +19,7 @@ const Accordion = () => {
 		} else {
 			cpyMultiple.splice(findIndexOfCurrentId, 1);
 		}
-
 		setMultiple(cpyMultiple);
-		// Stage 6 end
 	}
 
 	return (
@@ -32,25 +29,23 @@ const Accordion = () => {
 				className="mx-auto border text-3xl m-4 p-4 rounded-2xl cursor-pointer">
 				Enable Multiple Selection
 			</button>
-			
 
 			<div>
 				{data && data.length > 0 ? (
 					data.map((dataItem) => (
-						<div
-							key={dataItem.id}
-							className="border text-4xl text-center p-4">
+						<div key={dataItem.id} className="border text-4xl text-center p-4">
 							<div>
 								<h3>{dataItem.question}</h3>
-								<span onClick={
-									enableMultiSelect
-										? () => handleMultiSelection(dataItem.id)
-										: () => handleSelection(dataItem.id)
-								} className="cursor-pointer">+</span>
+								<span
+									onClick={
+										enableMultiSelect
+											? () => handleMultiSelection(dataItem.id)
+											: () => handleSelection(dataItem.id)
+									}
+									className="cursor-pointer">
+									+
+								</span>
 							</div>
-							{/* Stage 3: {selected === dataItem.id ? (
-								<div>{dataItem.answer}</div>
-							) : null} */}
 							{selected === dataItem.id ||
 							multiple.indexOf(dataItem.id) !== -1 ? (
 								<div>{dataItem.answer}</div>
