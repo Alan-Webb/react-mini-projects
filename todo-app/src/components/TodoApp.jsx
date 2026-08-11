50396b3ecbe20dd43e0f201a2a1144dd5ceed8c1
Alan Webb <webbalan91939@gmail.com>
2026-08-11 06:25:10 +0100

    Update TodoApp

--- todo-app/src/components/TodoApp.jsx
+++ todo-app/src/components/TodoApp.jsx
@@ -1,5 +1,36 @@
+import {useState} from "react";
+
 const TodoApp = () => {
-	return <div>TodoApp</div>;
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const handleAddTask = () =>{
+
+	}
+
+	return (
+		<div className="flex flex-col items-center text-2xl mt-12">
+			<div className="flex space-x-3">
+				<input
+					type="text"
+					name="add-todo"
+					placeholder="Enter your task..."
+					className="border rounded-xl p-3"
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+				/>
+				<button
+				onClick={handleAddTask}
+				className="border rounded-xl p-3 cursor-pointer	">
+					Add Todo
+				</button>
+			</div>
+			<div>
+				Todo List
+				<div>Todo Item</div>
+			</div>
+		</div>
+	);
 };
 
 export default TodoApp;
